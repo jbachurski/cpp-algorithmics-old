62b964de230c5f6b6bb7090f336723b2d6893618
KubinGH <KubinGH@users.noreply.github.com>
2017-12-27 19:36:16 +0100

    2017-12-25; group files

--- AlgorithmicsSet/stat.py
+++
@@ -1,9 +0,0 @@
-import os
-import collections
-c = collections.Counter()
-for filename in os.listdir():
-    if filename in __file__:
-        continue
-    c[filename[0]] += 1
-print(c.most_common(), sum(c.values()))
-input()
